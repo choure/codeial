32e97155690fc9ab11f7a9226124735873280d2d
Mohit Choure <mohitchoure1234@gmail.com>
2024-02-20 23:14:05 +0530

    Use static to access static folder and give layout test styles

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+
+//use static files above layout
+app.use(express.static('./assets'));
+
+//we need to use layouts before calling any other routes,
+//To tell app that we are using some sort of layout
+app.use(expressLayouts);
 
 //use express router
 app.use('/', require('./routes'));
