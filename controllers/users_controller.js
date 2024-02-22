3f3e32274a00445ca3926bfbbaea6df4e25cc485
Mohit Choure <mohitchoure1234@gmail.com>
2024-02-22 14:52:47 +0530

    Render pages for Sign up and Sign in

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -7,3 +7,15 @@ module.exports.profile = function(req, res){
 module.exports.createProfile = function(req, res){
     res.end('<h1>Create Profile</h1>');
 }
+
+module.exports.signup = function(req, res){
+    return res.render('user_sign_up',{
+        title: "Codeial | Sign Up"
+    });
+}
+
+module.exports.signin = function(req, res){
+    return res.render('user_sign_in',{
+        title: "Codeial | Sign In"
+    });
+}
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,10 @@ const router = express.Router();
 const usersRouter = require('../controllers/users_controller');
 
 router.get('/profile', usersRouter.profile);
+
 router.get('/create_profile', usersRouter.createProfile);
+
+router.get('/sign-up', usersRouter.signup);
+router.get('/sign-in', usersRouter.signin);
 
 module.exports = router;
