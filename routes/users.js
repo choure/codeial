b13a0d95a8480c7eacc0ac5d754ef23383d9ff29
Mohit Choure <mohitchoure1234@gmail.com>
2024-02-28 19:14:12 +0530

    Set current authenticated user

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ const usersRouter = require('../controllers/users_controller');
 
 const passport = require('passport');
 
-router.get('/profile', usersRouter.profile);
+router.get('/profile', passport.checkAuthentication, usersRouter.profile);
 
 router.get('/sign-up', usersRouter.signup);
 router.get('/sign-in', usersRouter.signin);
