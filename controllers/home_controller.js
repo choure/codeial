686e9376de3f7d26b09a2971aaa5c5a0470ee908
Mohit Choure <mohitchoure1234@gmail.com>
2024-03-11 20:08:25 +0530

    Convert home_controller to Async Await and showing profile only to signed-in user in footer

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,26 +1,31 @@
 const Post = require('../models/post');
 const User = require('../models/user');
 
-module.exports.home = function (req, res){
+module.exports.home = async function (req, res){
     //console.log(req.cookies);
     //res.cookie('user_id', 25);
 
-    //populate user of each post
-    Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    })
-    .then(function(posts){
-        User.find({}).then(function(users){
-            return res.render('home', {
-                title: "Codeial | Home",
-                posts: posts,
-                all_users: users
-            });
-        }).catch(function(err){console.log('Error in finding all users: ', err);});
-    }).catch(err => {console.log('Error in finding all posts: ', err);});
+    try{
+        //populate user of each post
+        let posts = await Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        }); 
+
+        let users = await User.find({});
+
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts: posts,
+            all_users: users
+        });
+    }catch(err){
+        console.log('Error: ' + err);
+        return;
+    }
+
 }
